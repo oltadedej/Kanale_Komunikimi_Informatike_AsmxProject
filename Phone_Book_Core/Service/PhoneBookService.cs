6aaf9b9da02d4995dc417b6abaffcec74d981631
Olta DEDEJ_EXT <olta.dedej_ext@alten.it>
2020-11-26 09:50:09 +0100

    delete fizik

--- Phone_Book_Core/Service/PhoneBookService.cs
+++ Phone_Book_Core/Service/PhoneBookService.cs
@@ -107,9 +107,40 @@ namespace Phone_Book_Core.Service
                     using (PHONE_BOOK_DBEntities db = new PHONE_BOOK_DBEntities())
                     {
                         item.DELETED_DATE = DateTime.Now;
-                        db.T_PHONE_BOOK.Add(item); // Delete fizik
-                         // db.Entry(item).State = System.Data.Entity.EntityState.Deleted; // delete fizik
+                        db.T_PHONE_BOOK.Add(item); 
                         db.Entry(item).State = System.Data.Entity.EntityState.Modified; // delete logjik
+                        db.SaveChanges();
+                        isDeleted = true;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    // write logs into file logs
+                }
+
+            }
+
+            return isDeleted;
+        }
+
+        /// <summary>
+        /// Kjo metode tenton te realizoje nje delete fizik mbi bazen e te dhenave per objektin e percaktuar
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool DeleteFizik(T_PHONE_BOOK item)
+        {
+            bool isDeleted = false;
+            if (item != null)
+            {
+                try
+                {
+                    using (PHONE_BOOK_DBEntities db = new PHONE_BOOK_DBEntities())
+                    {
+                        
+                        db.T_PHONE_BOOK.Remove(item);
                         db.SaveChanges();
                         isDeleted = true;
                     }
